294101c23dc4af0a1329305f70ad06a6c997210d
Sergio Serrano <ssergiocr01@yahoo.com>
2023-06-05 19:07:47 -0600

    Adding multilevel crud of countries, states and cities

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -28,7 +28,7 @@ namespace Sales.API.Controllers
         [HttpGet("{id:int}")]
         public async Task<ActionResult> Get(int id)
         {
-            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            var country = await _context.Countries.Include(x => x.States).FirstOrDefaultAsync(x => x.Id == id);
             if (country is null) 
             {
                 return NotFound();
---
+++ Sales.API/Controllers/StatesController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public StatesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.States
+                .Include(x => x.Cities)
+                .ToListAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var state = await _context.States
+                .Include(x => x.Cities)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(state);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(State state)
+        {
+            try
+            {
+                _context.Add(state);
+                await _context.SaveChangesAsync();
+                return Ok(state);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe un estado/departamento con el mismo nombre");
+                }
+
+                return BadRequest(dbUpdateException.Message);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync(State state)
+        {
+            try
+            {
+                _context.Update(state);
+                await _context.SaveChangesAsync();
+                return Ok(state);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe un estado/departamento con el mismo nombre");
+                }
+
+                return BadRequest(dbUpdateException.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var state = await _context.States.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(state);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
